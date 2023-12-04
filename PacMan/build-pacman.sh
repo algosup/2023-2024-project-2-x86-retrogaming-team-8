74e2709d9c2ee50ea0c592d4064a9d21e8298210
David Cuahonte Cuevas <91249658+DavidCC812@users.noreply.github.com>
2023-12-04 09:37:37 +0100

    renamed Folder

--- PacMan/build-pacman.sh
+++ PacMan/build-pacman.sh
@@ -3,7 +3,7 @@ set -Cue
 
 thisDir="$(cd "$(dirname "$0")" && pwd)"
 rootDir="$(cd "${thisDir}/../../.." && pwd)"
-binDir="${rootDir}/git/2023-2024-project-2-x86-retrogaming-team-8/assembly/bin"
+binDir="${rootDir}/git/2023-2024-project-2-x86-retrogaming-team-8/PacMan/bin"
 
 echo "$binDir"
 
