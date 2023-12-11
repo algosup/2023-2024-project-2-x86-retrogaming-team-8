b6ae65856f123b6624316abb6dc7c51fa4b4058f
NanaChocolat <145995847+NanaChocolat@users.noreply.github.com>
2023-12-11 10:40:25 +0100

    maze implemented

--- PacMan/pacman.asm
+++ PacMan/pacman.asm
@@ -1,287 +1,344 @@
-
 org 100h
+
+%include "sprite.asm"
 
 section .data
 
-    %define SCREEN_WIDTH 320
-    %define SCREEN_HEIGHT 200
-    spritew dw 8
-    spriteh dw 8
-    %define SPRITEW 8
-    %define SPRITEH 8
-    backBufferSeg resw 1
-    xPos dw 0     ; Initial x position
-    yPos dw 0     ; Initial y position
-    xVelocity dw 1  ; Initial x velocity
-    yVelocity dw 0  ; Initial y velocity
-    actualKeystroke dw 4Dh
-
-    Pacman db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x2C, 0x2C, 0x2C, 0x2C, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-           db 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
-           ;Pacman sprite
+    ; maze array
+tilemap  db 26,22,22,22,22,22,22,22,22,22,22,22,22,30,31,22,22,22,22,22,22,22,22,22,22,22,22,27
+         db 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,24
+         db 25, 0,14,10,10,15, 0,14,10,10,10,15, 0,13,12, 0,14,10,10,10,15, 0,14,10,10,15, 0,24
+         db 25, 0,13, 0, 0,12, 0,13, 0, 0, 0,12, 0,13,12, 0,13, 0, 0, 0,12, 0,13, 0, 0,12, 0,24
+         db 25, 0,17,11,11,16, 0,17,11,11,11,16, 0,17,16, 0,17,11,11,11,16, 0,17,11,11,16, 0,24
+         db 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,24
+         db 25, 0,14,10,10,15, 0,14,15, 0,14,10,10,10,10,10,10,15, 0,14,15, 0,14,10,10,15, 0,24
+         db 25, 0,17,11,11,16, 0,13,12, 0,17,11,11,19,18,11,11,16, 0,13,12, 0,17,11,11,16, 0,24
+         db 25, 0, 0, 0, 0, 0, 0,13,12, 0, 0, 0, 0,13,12, 0, 0, 0, 0,13,12, 0, 0, 0, 0, 0, 0,24
+         db 29,23,23,23,23,15, 0,13,21,10,10,15, 0,13,12, 0,14,10,10,20,12, 0,14,23,23,23,23,28
+         db  0, 0, 0, 0, 0,25, 0,13,18,11,11,16, 0,17,16, 0,17,11,11,19,12, 0,24, 0, 0, 0, 0, 0
+         db  0, 0, 0, 0, 0,25, 0,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,13,12, 0,24, 0, 0, 0, 0, 0
+         db  0, 0, 0, 0, 0,25, 0,13,12, 0,38,23,36, 1, 1,37,23,39, 0,13,12, 0,24, 0, 0, 0, 0, 0
+         db 22,22,22,22,22,16, 0,17,16, 0,24, 0, 0, 0, 0, 0, 0,25, 0,17,16, 0,17,22,22,22,22,22
+         db  0, 0, 0, 0, 0, 0, 0, 0, 0, 0,24, 0, 0, 0, 0, 0, 0,25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+         db 23,23,23,23,23,15, 0,14,15, 0,24, 0, 0, 0, 0, 0, 0,25, 0,14,15, 0,14,23,23,23,23,23
+         db  0, 0, 0, 0, 0,25, 0,13,12, 0,41,22,22,22,22,22,22,40, 0,13,12, 0,24, 0, 0, 0, 0, 0
+         db  0, 0, 0, 0, 0,25, 0,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,13,12, 0,24, 0, 0, 0, 0, 0
+         db  0, 0, 0, 0, 0,25, 0,13,12, 0,14,10,10,10,10,10,10,15, 0,13,12, 0,24, 0, 0, 0, 0, 0
+         db 26,22,22,22,22,16, 0,17,16, 0,17,11,11,19,18,11,11,16, 0,17,16, 0,17,22,22,22,22,27
+         db 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,24
+         db 25, 0,14,10,10,15, 0,14,10,10,10,15, 0,13,12, 0,14,10,10,10,15, 0,14,10,10,15, 0,24
+         db 25, 0,17,11,19,12, 0,17,11,11,11,16, 0,17,16, 0,17,11,11,11,16, 0,13,18,11,16, 0,24
+         db 25, 0, 0, 0,13,12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,13,12, 0, 0, 0,24
+         db 33,10,15, 0,13,12, 0,14,15, 0,14,10,10,10,10,10,10,15, 0,14,15, 0,13,12, 0,14,10,34
+         db 32,11,16, 0,17,16, 0,13,12, 0,17,11,11,19,18,11,11,16, 0,13,12, 0,17,16, 0,17,11,35
+         db 25, 0, 0, 0, 0, 0, 0,13,12, 0, 0, 0, 0,13,12, 0, 0, 0, 0,13,12, 0, 0, 0, 0, 0, 0,24
+         db 25, 0,14,10,10,10,10,20,21,10,10,15, 0,13,12, 0,14,10,10,20,21,10,10,10,10,15, 0,24
+         db 25, 0,17,11,11,11,11,11,11,11,11,16, 0,17,16, 0,17,11,11,11,11,11,11,11,11,16, 0,24
+         db 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,24
+         db 29,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,28
+   
+    Column dw 28    
+    Row dw 31       
+    pixelNumber dw 1920 
 
 section .text
+    mov ah, 00h     
+    mov al, 13h     
+    int 10h         
 
-mov ax, 0013h      ; setting the video mode for 640x480 screen and getting 256 colors. screen can be expanded without problem
-int 10h
+  call clearScreen    
 
-GameLoop:
-    mov al, 0FFh
-    call clearScreen
-    call wait_key_loop
+    gameLoop:
+    call tileMap
 
-
-    ; Check for keypress
-    mov ah, 01h
-    int 16h
-
-; AH = 0 if no key pressed, AH = ASCII code if key pressed
-;jz NoKeyPress
-
-; Check for key and update position
-
-
-read_character_key_was_pressed:
-    mov ah, 01h        ; Check if a key has been pressed (non-blocking)
-    int 16h
-    jz continue_movement ; If no key pressed, continue current movement
-
-    ; Read the keystroke
-    mov ah, 00h
-    int 16h
-    cmp ah, 4Dh
-    je update_keystroke
-    cmp ah, 4Bh
-    je update_keystroke
-    cmp ah, 48h
-    je update_keystroke
-    cmp ah, 50h
-    je update_keystroke
-    jmp continue_movement
-
-update_keystroke:
-    mov [actualKeystroke], ah  ; Store the new direction
-    jmp continue_movement
-
-continue_movement:
-    mov al, [actualKeystroke]
-    cmp al, 4Dh
-    je .move_right
-    cmp al, 4Bh
-    je .move_left
-    cmp al, 48h
-    je .move_up
-    cmp al, 50h
-    je .move_down
-    ret
-    .move_right:
-        call move_right
+    drawWalls:
+        ; draw 1 tile
+        mov dx, 6           
+        .eachLine:  
+            mov cx, 6       
+            rep movsb       
+            add di, 320-6   
+            dec dx          
+            jnz .eachLine   
+            call nextColumn 
+        inc bx              
         ret
-    .move_left:
-        call move_left
-        ret
-    .move_up:
-        call move_up
-        ret
-    .move_down:
-        call move_down
-        ret
-    ret
 
-move_right:
-    mov word [actualKeystroke], 4Dh
-    call Pacman   ; animation
-    mov bx, [xPos]
-    add bx, 3/2
-    cmp bx, SCREEN_WIDTH - SPRITEW 
-    jae .skip_move_right
-    mov [xPos], bx
-.skip_move_right:
-    ret
-move_left:
-    mov word [actualKeystroke], 4Bh
-    call Pacman   ; animation
-    mov bx, [xPos]
-    sub bx, 3/2
-    cmp bx, 0
-    jbe .skip_move_left
-    mov [xPos], bx
-.skip_move_left:
-    ret
+    nextColumn:
+        mov ax, [Column]   
+        dec ax              
+        mov [Column], ax    
+        cmp ax, 0           
+        je nextRow         
+        sub di, 1914        
+        ret
 
-move_up:
-    mov word [actualKeystroke], 48h
-    call Pacman      ; animation
-    mov bx, [yPos]
-    sub bx, 3/2
-    cmp bx, 0
-    jbe .skip_move_up
-    mov [yPos], bx
-.skip_move_up:
-    ret
+    nextRow:
+        mov ax, [Row]       
+        dec ax             
+        mov [Row], ax       
+        cmp ax, 0           
+        je end             
+        mov ax, 28          
+        mov [Column], ax    
+        mov ax, [pixelNumber] 
+        mov di, ax          
+        add ax, 1920       
+        mov [pixelNumber], ax   ;
+        ret
 
-move_down:
-    mov word [actualKeystroke], 50h
-    call Pacman   ; animation
-    mov bx, [yPos]
-    add bx, 3/2
-    cmp bx, SCREEN_HEIGHT - SPRITEH 
-    jae .skip_move_down
-    mov [yPos], bx
+    tileMap:
+        mov di, 0              
+        mov bx, tilemap           
+        wallchoice:
+            mov al, [bx]       
+            cmp al, 0          
+            je empty        
+            cmp al, 1
+            je roomGhost
+            cmp al, 10
+            je wall1
+            cmp al, 11
+            je wall2
+            cmp al, 12
+            je wall3
+            cmp al, 13
+            je wall4
+            cmp al, 14
+            je angleWall1
+            cmp al, 15
+            je angleWall2
+            cmp al, 16
+            je angleWall3
+            cmp al, 17
+            je angleWall4
+            cmp al, 18
+            je angleWall5
+            cmp al, 19
+            je angleWall6
+            cmp al, 20
+            je angleWall7
+            cmp al, 21
+            je angleWall8
+            cmp al, 22
+            je wallDouble1
+            cmp al, 23
+            je wallDouble2
+            cmp al, 24
+            je wallDouble3
+            cmp al, 25
+            je wallDouble4
+            cmp al, 26
+            je angleDouble1
+            cmp al, 27
+            je angleDouble2
+            cmp al, 28
+            je angleDouble3
+            cmp al, 29
+            je angleDouble4
+            cmp al, 30
+            je angleDifDouble1
+            cmp al, 31
+            je angleDifDouble2
+            cmp al, 32
+            je angleDifDouble3
+            cmp al, 33
+            je angleDifDouble4
+            cmp al, 34
+            je angleDifDouble5
+            cmp al, 35
+            je angleDifDouble6
+            cmp al, 36
+            je doubleDoor1
+            cmp al, 37
+            je doubleDoor2
+            cmp al, 38
+            je straightDouble1
+            cmp al, 39
+            je straightDouble2
+            cmp al, 40
+            je straightDouble3
+            cmp al, 41
+            je straightDouble4
+        ret
+
+    empty:                  
+        mov si, EmptySpace           
+        call drawWalls          
+        jmp wallchoice         
+
+    roomGhost:
+        mov si, GhostRoom
+        call drawWalls
+        jmp wallchoice
+
+    wall1:              
+        mov si, WallBasic1
+        call drawWalls
+        jmp wallchoice
+
+    wall2:
+        mov si, WallBasic2
+        call drawWalls
+        jmp wallchoice
+
+    wall3:
+        mov si, WallBasic3
+        call drawWalls
+        jmp wallchoice
+
+    wall4:
+        mov si, WallBasic4
+        call drawWalls
+        jmp wallchoice
+
+    angleWall1:
+        mov si, AngleBasic1
+        call drawWalls
+        jmp wallchoice
+
+    angleWall2:
+        mov si, AngleBasic2
+        call drawWalls
+        jmp wallchoice
+
+    angleWall3:
+        mov si, AngleBasic3
+        call drawWalls
+        jmp wallchoice
+
+    angleWall4:
+        mov si, AngleBasic4
+        call drawWalls
+        jmp wallchoice
+
+    angleWall5:
+        mov si, AngleBasic5
+        call drawWalls
+        jmp wallchoice
+
+    angleWall6:
+        mov si, AngleBasic6
+        call drawWalls
+        jmp wallchoice
+
+    angleWall7:
+        mov si, AngleBasic7
+        call drawWalls
+        jmp wallchoice
+
+    angleWall8:
+        mov si, AngleBasic8
+        call drawWalls
+        jmp wallchoice
+
+    wallDouble1:
+        mov si, DoubleWall1
+        call drawWalls
+        jmp wallchoice
+
+    wallDouble2:
+        mov si, DoubleWall2
+        call drawWalls
+        jmp wallchoice
+
+    wallDouble3:
+        mov si, DoubleWall3
+        call drawWalls
+        jmp wallchoice
+
+    wallDouble4:
+        mov si, DoubleWall4
+        call drawWalls
+        jmp wallchoice
+
+    angleDouble1:
+        mov si, AngleDoubleWall1
+        call drawWalls
+        jmp wallchoice
+
+    angleDouble2:
+        mov si, AngleDoubleWall2
+        call drawWalls
+        jmp wallchoice
+
+    angleDouble3:
+        mov si, AngleDoubleWall3
+        call drawWalls
+        jmp wallchoice
+
+    angleDouble4:
+        mov si, AngleDoubleWall4
+        call drawWalls
+        jmp wallchoice
+
+    angleDifDouble1:
+        mov si, OtherAngleDoubleWall1
+        call drawWalls
+        jmp wallchoice
+
+    angleDifDouble2:
+        mov si, OtherAngleDoubleWall2
+        call drawWalls
+        jmp wallchoice
+
+    angleDifDouble3:
+        mov si, OtherAngleDoubleWall3
+        call drawWalls
+        jmp wallchoice
+
+    angleDifDouble4:
+        mov si, OtherAngleDoubleWall4
+        call drawWalls
+        jmp wallchoice
+
+    angleDifDouble5:
+        mov si, OtherAngleDoubleWall5
+        call drawWalls
+        jmp wallchoice
+
+    angleDifDouble6:
+        mov si, OtherAngleDoubleWall6
+        call drawWalls
+        jmp wallchoice
+
+    doubleDoor1:
+        mov si, DoorDoubleWall1
+        call drawWalls
+        jmp wallchoice
+
+    doubleDoor2:
+        mov si, DoorDoubleWall2
+        call drawWalls
+        jmp wallchoice
+
+    straightDouble1:
+        mov si, DoubleStraightAngle1
+        call drawWalls
+        jmp wallchoice
     
-.skip_move_down:
-    ret
+    straightDouble2:
+        mov si, DoubleStraightAngle2
+        call drawWalls
+        jmp wallchoice
 
+    straightDouble3:
+        mov si, DoubleStraightAngle3
+        call drawWalls
+        jmp wallchoice
 
-wait_key_loop:
-;       cmp ah, 48h ;if button "up arrow" pressed go to the programm MoveUp
- ;      je MoveUp
-  ;     cmp ah, 50h ;if button "down arrow" pressed go to the programm MoveDown
-   ;    je MoveDown
-    ;   cmp ah, 4Bh ;if button "left arrow" pressed go to the programm MoveLeft
-     ;  je MoveLeft
-      ; cmp ah, 4Dh ;if button "right arrow" pressed go to the programm MoveRight
-       ;je MoveRight
-       ;jne wait_key_loop
-NoKeyPress:
-    ; Update Pacman position based on velocity
-    ;mov di, word [yPos]      ; Use yPos for vertical position
-    ;mov si, Pacman
-    ;call printplayer
-    ;call presentBackBuffer
+    straightDouble4:
+        mov si, DoubleStraightAngle4
+        call drawWalls
+        jmp wallchoice
+    
+        clearScreen:
+        mov ax, 0xA000
+        mov es, ax
+        mov di, 0
+        mov cx, 320*200
+        rep stosb
+        ret 
 
-    ;mov bx, word [xPos]
-    ;add bx, word [xVelocity]
-    ;mov word [xPos], bx
-
-    ;mov bx, word [yPos]
-    ;add bx, word [yVelocity]
-    ;mov word [yPos], bx
-
-    ; Wait loop
-    ;mov cx, 10000
-
-    WaitLoop:
-        ;loop WaitLoop
-
-    ; Check boundaries and change velocity if necessary
-    ;cmp word [xPos], 320-16
-    ;je ChangeVelocityX
-    ;cmp word [xPos], 0
-    ;je ChangeVelocityX
-
-    ;cmp word [yPos], 200-16
-    ;je ChangeVelocityY
-    ;cmp word [yPos], 0
-    ;je ChangeVelocityY
-
-    ;jmp GameLoop
-
-MoveUp:
-    ;add word [yVelocity], 1
-    ;jmp NoKeyPress
-
-MoveDown:
-
-    ;sub word [yVelocity], 1
-    ;jmp NoKeyPress
-
-MoveLeft:
-    ;sub word [xVelocity], 1
-    ;jmp NoKeyPress
-
-MoveRight:
-    ;add word [xVelocity], 1
-    ;jmp NoKeyPress
-
-ChangeVelocityX:
-    ;neg word [xVelocity]
-    ;jmp GameLoop
-
-ChangeVelocityY:
-    ;neg word [yVelocity]
-    ;jmp GameLoop
-
-Exit:
-    mov ax, 0C01h ; 
-    int 21h
-
-    ; DOS box default video mode
-    mov ax, 03h 
-    int 21h
-
-    int 20h ; Quit
-
-
-
-
-
-the_functions:
-
-; need to set the color of filling in al
-clearScreen: ;this part of the programm will clean with a rapid speed
-mov ax, [cs:backBufferSeg]; backbufferSeg is needed for making sure that the cleaning doesn't provoke flickering !
-mov es, ax
-mov di, 0
-mov cx, 320*200
-rep stosb
-ret 
-
-presentBackBuffer:
-push ds
-push es
-mov ax, [cs:backBufferSeg]
-mov ds, ax
-mov ax, 0A000h
-mov es, ax
-xor si, si
-xor di, di
-mov cx, 320*200
-rep movsb
-op es
-pop ds
-ret
-
-loopy5:
-    mov cx, 16
-    rep movsb
-    add di, 640-16
-    dec dx
-    jnz loopy5
-ret
-
-printplayer:
-    mov ax, [cs:backBufferSeg]
-    mov es, ax
-    mov dx, 16
-
-    cld  ; Clear direction flag
-
-    loopyy:
-        mov cx, 16
-        rep movsb
-        add di, 320-16
-        dec dx
-        jnz loopyy
-    ret
-
-
-ChangeVelocity:
-neg word [xVelocity]
-jmp GameLoop
-
-%include "seg.inc"
+    end:
+        
