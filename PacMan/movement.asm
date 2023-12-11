e1fb48fac239ba537abbe52b1a408ffa1658a3de
Mattstar64 <146164921+Mattstar64@users.noreply.github.com>
2023-12-11 11:46:23 +0100

    fixing comments

--- PacMan/movement.asm
+++ PacMan/movement.asm
@@ -1,9 +1,10 @@
 org 100h
 
-%include "sprite.asm"
 ;getting the code of the sprite bitmaps
-section .data
+%include "sprite.asm"
 
+section .data
+;sprite that have the same size as the pacman sprite that will pass behind pacman and so used as an eraser of old position sprite
              emptyspace db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                         db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                         db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -14,7 +15,7 @@ section .data
                         db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                         db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                         db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-;sprite that have the same size as the pacman sprite that will pass behind pacman and so used as an eraser of old position sprite
+
 
 ;Default position
 position dw 23000
@@ -31,20 +32,27 @@ call clearScreen
 
 gameLoop:
     call waitLoop
+    ; Wait for keypress
     mov ah, 01h
     int 16h
     jz gameLoop
     mov ah, 00h
-    int 16h ; Wait for keypress
-    cmp ah, 48h ; If the key "UParrow" is pressed then jump to MoveUp label
+    int 16h 
+    ; If the key "UParrow" is pressed then jump to MoveUp label
+    cmp ah, 48h 
     je moveUp
-    cmp ah, 4Bh ; If the key "LEFTarrow" is pressed then jump to MoveLeft label
+    ; If the key "LEFTarrow" is pressed then jump to MoveLeft label
+    cmp ah, 4Bh 
     je moveLeft
-    cmp ah, 4Dh ; If the key "RIGHTarrow" is pressed then jump to MoveRight label
+    ; If the key "RIGHTarrow" is pressed then jump to MoveRight label
+    cmp ah, 4Dh 
     je moveRight
-    cmp ah, 50h ; If the key "DOWNarrow" is pressed then jump to Movedown label
+    ; If the key "DOWNarrow" is pressed then jump to Movedown label
+    cmp ah, 50h 
     je moveDown
 
+
+; Clear the screen
 clearScreen: 
     mov ax, 0xA000
     mov es, ax
@@ -52,14 +60,17 @@ clearScreen:
     mov cx, 320*200
     rep stosb
     ret
-    ; Clear the screen
 
+
+
+; Clear the ancient pacman sprite by using the emptyspace sprite
 clearPacMan:
     mov di, [position]
     mov si, emptyspace
     call DrawPacMan
     ret
-    ; Clear the ancient pacman sprite by using the emptyspace sprite
+
+; Printing line by line the PacMan using the postion
 drawPacMan:
     mov di, [position]
     mov dx, 10
@@ -71,39 +82,42 @@ drawPacMan:
           dec dx
           jnz eachLine
           ret
-    ; Printing line by line the PacMan using the postion
 
+
+; Change the movement to the left by calculating the position
 moveLeft:
     call clearPacMan
     mov si, pacmanChompLeft1
     sub word [position], 1*2
     call drawPacMan 
     jmp gameLoop
-    ; Change the movement to the left by calculating the position
 
+; Change the movement to the right by calculating the position
 moveRight:
     call clearPacMan
     mov si, pacmanChompRight1
     add word [position], 1*2
     call drawPacMan
     jmp gameLoop
-    ; Change the movement to the right by calculating the position
 
+
+; Change the movement to go up by calculating the position
 moveUp:
     call clearPacMan
     mov si, pacmanChompUp1
     sub word [position], 320*2
     call drawPacMan
     jmp gameLoop
-    ; Change the movement to go up by calculating the position
 
+
+; Change the movement to go down by calculating the position
 moveDown:
     call clearPacMan
     mov si, pacmanChompDown1
     add word [position], 320*2
     call drawPacMan
     jmp gameLoop
-    ; Change the movement to go down by calculating the position
+    
 
 waitLoop:
     loop waitLoop
