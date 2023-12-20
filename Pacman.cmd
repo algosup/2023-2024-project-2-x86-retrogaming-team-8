ae7f46b32a4db2db6656a8dc4051beaa7ddd9a3e
Mattstar64 <146164921+Mattstar64@users.noreply.github.com>
2023-12-20 09:47:57 +0100

    fixing glitches

--- Pacman.cmd
+++ Pacman.cmd
@@ -4,7 +4,5 @@ set "BIN_DIR=%ROOT_DIR%bin"
 set "DOSBOX_BIN=%ProgramFiles(x86)%\DOSBox-0.74-3\DOSBox.exe"
 set "CONFIG_LOC=%ROOT_DIR%"
 
-convert "%ROOT_DIR%icon.ico" "%ROOT_DIR%icon.png"
-nasm -f bin "%ROOT_DIR%icon.asm" -o "%BIN_DIR%icon.bin"
 
 "%DOSBOX_BIN%" -c "MOUNT c %BIN_DIR%" -c "c:" -c "main.com"
